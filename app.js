ccf89d30ce2efe2a3ea8e6473e54641af59fdea9
enkog <nkirukaogu@gmail.com>
2021-08-09 20:46:45 +0100

    Fix linter errors

--- app.js
+++ app.js
@@ -4,67 +4,66 @@ mainBookContainer.className = 'main-book-container';
 const addBtn = document.querySelector('.add-btn');
 
 const bookArray = JSON.parse(localStorage.getItem('Books')) || [
-    {title: "The lord of the rings", author: "Harry Potter"},
-    {title: "Things fall apart", author: "Chinua Achebe"},
+  { title: 'The lord of the rings', author: 'Harry Potter' },
+  { title: 'Things fall apart', author: 'Chinua Achebe' },
 ];
 
-const removeBook = function(index) {
-    const books = bookArray.filter((book, i) => i !== index);
-    localStorage.setItem('Books', JSON.stringify(books));
-    const parent = document.getElementById(index);
-    parent.parentNode.removeChild(parent);
-}
+const removeBook = function (index) {
+  const books = bookArray.filter((book, i) => i !== index);
+  localStorage.setItem('Books', JSON.stringify(books));
+  const parent = document.getElementById(index);
+  parent.parentNode.removeChild(parent);
+};
 
-const createBookElement = function(title, author, index) {   
-    const bookContainer = document.createElement('div');
-    bookContainer.className = 'book-container';
-    bookContainer.id = index;
+const createBookElement = function (title, author, index) {
+  const bookContainer = document.createElement('div');
+  bookContainer.className = 'book-container';
+  bookContainer.id = index;
 
-    const titleDesc = document.createElement('div');
-    titleDesc.className = 'title mb-2';
-    titleDesc.textContent = title;
+  const titleDesc = document.createElement('div');
+  titleDesc.className = 'title mb-2';
+  titleDesc.textContent = title;
 
-    const authorDesc = document.createElement('div');
-    authorDesc.className = 'author mb-2';
-    authorDesc.textContent = author;
+  const authorDesc = document.createElement('div');
+  authorDesc.className = 'author mb-2';
+  authorDesc.textContent = author;
 
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = 'Remove';
-    removeBtn.addEventListener('click', removeBook.bind(null, index));
-    removeBtn.className = 'remove-btn btn btn-secondary';
-    const removeBtnId = `remove-btn${index}`;
-    removeBtn.id = removeBtnId;
-   
-    const hrElement = document.createElement('hr');
-    hrElement.className = 'w-50';
+  const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
+  removeBtn.addEventListener('click', removeBook.bind(null, index));
+  removeBtn.className = 'remove-btn btn btn-secondary';
+  const removeBtnId = `remove-btn${index}`;
+  removeBtn.id = removeBtnId;
 
-    bookContainer.appendChild(titleDesc);
-    bookContainer.appendChild(authorDesc);
-    bookContainer.appendChild(removeBtn);
-    bookContainer.appendChild(hrElement);
-    mainBookContainer.appendChild(bookContainer);
-    
-}
+  const hrElement = document.createElement('hr');
+  hrElement.className = 'w-50';
 
-const addBook = function() {
+  bookContainer.appendChild(titleDesc);
+  bookContainer.appendChild(authorDesc);
+  bookContainer.appendChild(removeBtn);
+  bookContainer.appendChild(hrElement);
+  mainBookContainer.appendChild(bookContainer);
+};
+
+const addBook = function () {
   const titleInputValue = document.getElementById('title-input').value;
   const authorInputValue = document.getElementById('author-input').value;
 
-  const newBook = {title: titleInputValue, author: authorInputValue};
+  const newBook = { title: titleInputValue, author: authorInputValue };
 
   const bookArrLength = bookArray.push(newBook);
 
   createBookElement(titleInputValue, authorInputValue, bookArrLength);
   localStorage.setItem('Books', JSON.stringify(bookArray));
-}
+};
 
 addBtn.addEventListener('click', addBook);
 
-const displayBooks = function(arr) {
-    bookArray.forEach((element, index) => {
-        createBookElement(element.title, element.author, index);
-    })
-    containerDiv.appendChild(mainBookContainer);
-}
+const displayBooks = function (arr) {
+  arr.forEach((element, index) => {
+    createBookElement(element.title, element.author, index);
+  });
+  containerDiv.appendChild(mainBookContainer);
+};
 
 displayBooks(bookArray);
