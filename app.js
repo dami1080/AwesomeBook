131673c820ad01817b58acaff38ee9eed400d1de
enkog <nkirukaogu@gmail.com>
2021-08-09 14:35:31 +0100

    Add function to create a new book element

--- app.js
+++ app.js
@@ -3,9 +3,35 @@ const bookArray = [
     {title: "go", author: "dare"},
 ];
 
+const createBookElement = function(title, author) {
+    const containerDiv = document.querySelector('.container-div');
+    const mainBookContainer = document.createElement('div');
+    mainBookContainer.className = 'main-book-container';
+
+    const titleDesc = document.createElement('div');
+    titleDesc.className = 'title';
+    titleDesc.textContent = title;
+
+    const authorDesc = document.createElement('div');
+    authorDesc.className = 'author';
+    authorDesc.textContent = author;
+
+    const removeBtn = document.createElement('button');
+    removeBtn.className = 'remove-btn';
+    removeBtn.textContent = 'Remove';
+
+    const hrElement = document.createElement('hr');
+
+    mainBookContainer.appendChild(titleDesc);
+    mainBookContainer.appendChild(authorDesc);
+    mainBookContainer.appendChild(removeBtn);
+    mainBookContainer.appendChild(hrElement);
+    containerDiv.appendChild(mainBookContainer);
+}
+
 const addBook = function() {
-  const titleInputValue = document.getElementById('title').value;
-  const authorInputValue = document.getElementById('author').value;
+  const titleInputValue = document.getElementById('title-input').value;
+  const authorInputValue = document.getElementById('author-input').value;
 
   const newBook = {title: titleInputValue, author: authorInputValue};
 
