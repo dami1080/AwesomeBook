f19dce77079d1f8f17043fd7ca088e6667a12465
enkog <nkirukaogu@gmail.com>
2021-08-09 14:19:52 +0100

    Add function to add a new book to the array

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const  bookArray = [
+const bookArray = [
     {title: "the lord of the rings", author: "Harry poter"},
     {title: "go", author: "dare"},
-
 ];
+
+const addBook = function() {
+  const titleInputValue = document.getElementById('title').value;
+  const authorInputValue = document.getElementById('author').value;
+
+  const newBook = {title: titleInputValue, author: authorInputValue};
+
+  bookArray.push(newBook);
+}
 
