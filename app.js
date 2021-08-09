077a041c1e08c6112956e726d90681d8cf2876af
enkog <nkirukaogu@gmail.com>
2021-08-09 20:07:32 +0100

    Update the remove functions and add basic style to page

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 const containerDiv = document.querySelector('.container-div');
-
 const mainBookContainer = document.createElement('div');
 mainBookContainer.className = 'main-book-container';
+const addBtn = document.querySelector('.add-btn');
 
-const bookArray = JSON.parse(localStorage.getItem('book')) || [
-    {title: "The lord of the rings", author: "Harry poter"},
+const bookArray = JSON.parse(localStorage.getItem('Books')) || [
+    {title: "The lord of the rings", author: "Harry Potter"},
     {title: "Things fall apart", author: "Chinua Achebe"},
 ];
 
+const removeBook = function(index) {
+    const books = bookArray.filter((book, i) => i !== index);
+    localStorage.setItem('Books', JSON.stringify(books));
+    const parent = document.getElementById(index);
+    parent.parentNode.removeChild(parent);
+}
 
-const createBookElement = function(title, author, index) {
+const createBookElement = function(title, author, index) {   
     const bookContainer = document.createElement('div');
     bookContainer.className = 'book-container';
     bookContainer.id = index;
 
     const titleDesc = document.createElement('div');
-    titleDesc.className = 'title';
+    titleDesc.className = 'title mb-2';
     titleDesc.textContent = title;
 
     const authorDesc = document.createElement('div');
-    authorDesc.className = 'author';
+    authorDesc.className = 'author mb-2';
     authorDesc.textContent = author;
 
     const removeBtn = document.createElement('button');
-    removeBtn.className = 'remove-btn';
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', removeBook.bind(null, index));
+    removeBtn.className = 'remove-btn btn btn-secondary';
     const removeBtnId = `remove-btn${index}`;
     removeBtn.id = removeBtnId;
-    removeBtn.textContent = 'Remove';
-
+   
     const hrElement = document.createElement('hr');
+    hrElement.className = 'w-50';
 
     bookContainer.appendChild(titleDesc);
     bookContainer.appendChild(authorDesc);
@@ -47,10 +55,9 @@ const addBook = function() {
   const bookArrLength = bookArray.push(newBook);
 
   createBookElement(titleInputValue, authorInputValue, bookArrLength);
-  localStorage.setItem('book', JSON.stringify(bookArray));
+  localStorage.setItem('Books', JSON.stringify(bookArray));
 }
 
-const addBtn = document.querySelector('.add-btn');
 addBtn.addEventListener('click', addBook);
 
 const displayBooks = function(arr) {
@@ -61,10 +68,3 @@ const displayBooks = function(arr) {
 }
 
 displayBooks(bookArray);
-
-const removeBook = function(index) {
-  const books = bookArray.filter((book, i) => i !== index);
-  
-  const parent = document.getElementById('index');
-  parent.remove();
-}
