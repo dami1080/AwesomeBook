dd7c769c913b5d3bae8f65890d546c2ecc3d92f0
dami1080 <dareakintoye@gmail.com>
2021-08-10 10:45:59 -1100

    Create the book class

---
+++ main.js
@@ -0,0 +1,11 @@
+ class Book {
+     constructor (title, author, index){
+         this.title = title;
+         this.author = author;
+         this.index = index;
+         bookArray = [
+            { title: 'The lord of the rings', author: 'Harry Potter' },
+            { title: 'Things fall apart', author: 'Chinua Achebe' },
+         ]
+     }
+}
