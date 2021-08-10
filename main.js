835900fbe8eba54e8a1c001d0a484c14796b95a0
dami1080 <dareakintoye@gmail.com>
2021-08-10 10:53:58 -1100

    Create add book method to the book class

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
  class Book {
-     constructor (title, author, index){
-         this.title = title;
-         this.author = author;
-         this.index = index;
+     constructor (){
          bookArray = [
             { title: 'The lord of the rings', author: 'Harry Potter' },
             { title: 'Things fall apart', author: 'Chinua Achebe' },
          ]
-     }
+    }
+   addBook() {
+    const titleInputValue = document.getElementById('title-input').value;
+    const authorInputValue = document.getElementById('author-input').value;
+  
+    const newBook = { title: titleInputValue, author: authorInputValue };
+  
+    const bookArrLength = bookArray.push(newBook);
+  
+    createBookElement(titleInputValue, authorInputValue, bookArrLength);
+    localStorage.setItem('Books', JSON.stringify(bookArray)); 
+   } 
 }
